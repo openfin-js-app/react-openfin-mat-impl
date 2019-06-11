444a8da673f7e2ea34a7586285afd32067964b04
Wentao LI <liwentao90@yahoo.com>
2019-06-11 05:39:56 -0400

    initially 5#
    - Update dependencies to
        - @material-ui/core @ ^4.1.0
        - @material-ui/pickers @ 3.1.0"
        - create-react-app @ 3.0.1
        - classnames @ ^2.2.6
        - chai @ ^4.2.0
    - Remove dependencies
        - material-ui-pickers @ ^2.2.4
    - Refactor buildMuiTheme parameters

--- src/views/ConfigView/ConfigView.tsx
+++ src/views/ConfigView/ConfigView.tsx
@@ -26,6 +26,9 @@ import CheckIcon from '@material-ui/icons/Check';
 
 import { makeStyles } from '@material-ui/styles';
 
+import {MuiPickersUtilsProvider} from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment'
+
 import {useTranslation} from 'react-i18next';
 
 import cx from 'classnames';
@@ -93,93 +96,95 @@ const ConfigView:React.FunctionComponent<IProps> = (
     }
 
     return (<React.Fragment>
-        <div className={classes.topInputContainer}>
-            <TextField
-                id={'filter-string-input'}
-                InputProps={{
-                    startAdornment:(
-                        <InputAdornment position={'start'}>
-                            <SearchIcon/>
-                        </InputAdornment>
-                    )
-                }}
-                InputLabelProps={{
-                    shrink:true,
-                }}
-                placeholder={t('common.globalFilterPlaceholder')}
-                fullWidth
-                onChange={handleGlobalFilterStrChange}
-                margin={'dense'}
-            />
-        </div>
-        <div className={classes.configContainer}>
-            <SplitterLayout primaryMinSize={200} secondaryMinSize={400} secondaryInitialSize={600}>
-                <div className={classes.configLeftSection}>
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+            <div className={classes.topInputContainer}>
+                <TextField
+                    id={'filter-string-input'}
+                    InputProps={{
+                        startAdornment:(
+                            <InputAdornment position={'start'}>
+                                <SearchIcon/>
+                            </InputAdornment>
+                        )
+                    }}
+                    InputLabelProps={{
+                        shrink:true,
+                    }}
+                    placeholder={t('common.globalFilterPlaceholder')}
+                    fullWidth
+                    onChange={handleGlobalFilterStrChange}
+                    margin={'dense'}
+                />
+            </div>
+            <div className={classes.configContainer}>
+                <SplitterLayout primaryMinSize={200} secondaryMinSize={400} secondaryInitialSize={600}>
+                    <div className={classes.configLeftSection}>
+                        <Scrollbars
+                            renderThumbVertical={props => <div className={"dark-thumb-vertical"} {...props}/>}
+                        >
+                            <List>
+                                {tabShownCnt>0?
+                                    tabs.map((oneTab:IConfigTab, index:number)=>{
+                                        if (globalFilterString===''|| oneTab._fieldLabels.toLowerCase().indexOf(globalFilterString.toLowerCase())>-1){
+                                            return(
+                                                <ListItem button className={cx('active',classes.configLeftListItem)} key={index} dense={true}
+                                                          onClick={handleTabItemClick(index)}
+                                                >
+                                                    <ListItemIcon>
+                                                        {oneTab._svgUrl?<img src={getTabIcon(oneTab)}/>:<oneTab._icon/>}
+                                                    </ListItemIcon>
+                                                    <ListItemText
+                                                        primary={t(oneTab._label)}
+                                                    />
+                                                    {currentTab===index?
+                                                        <ListItemSecondaryAction>
+                                                            <CheckIcon/>
+                                                        </ListItemSecondaryAction>
+                                                        :null}
+                                                </ListItem>
+                                            );
+                                        }else{
+                                            return null;
+                                        }
+                                    }):
+                                    <Paper>
+                                        <Typography variant='body2'>
+                                            Not found ...
+                                        </Typography>
+                                    </Paper>
+                                }
+                            </List>
+                        </Scrollbars>
+                    </div>
                     <Scrollbars
                         renderThumbVertical={props => <div className={"dark-thumb-vertical"} {...props}/>}
                     >
-                        <List>
-                            {tabShownCnt>0?
-                                tabs.map((oneTab:IConfigTab, index:number)=>{
-                                    if (globalFilterString===''|| oneTab._fieldLabels.toLowerCase().indexOf(globalFilterString.toLowerCase())>-1){
-                                        return(
-                                            <ListItem button className={cx('active',classes.configLeftListItem)} key={index} dense={true}
-                                                      onClick={handleTabItemClick(index)}
-                                            >
-                                                <ListItemIcon>
-                                                    {oneTab._svgUrl?<img src={getTabIcon(oneTab)}/>:<oneTab._icon/>}
-                                                </ListItemIcon>
-                                                <ListItemText
-                                                    primary={t(oneTab._label)}
+                        <div className={classes.configMainSection}>
+                            <GridList cellHeight={52} className={classes.configMainGridList} cols={12}>
+                                {filedShownCnt>0?
+                                    tabs[currentTab]._fields.map((oneField:IConfigField, index:number)=>(
+                                        globalFilterString === '' || ( t(oneField._label) as string ).toLowerCase().indexOf(globalFilterString.toLowerCase())>-1?
+                                            <GridListTile key={index} cols={oneField._cols || 3} rows={oneField._rows || 1}>
+                                                <ConfigFieldComp
+                                                    value={config[tabName][oneField._name]}
+                                                    onChange={handleUpdateOneField(tabName,oneField._name)}
+                                                    {...oneField}
                                                 />
-                                                {currentTab===index?
-                                                    <ListItemSecondaryAction>
-                                                        <CheckIcon/>
-                                                    </ListItemSecondaryAction>
-                                                    :null}
-                                            </ListItem>
-                                        );
-                                    }else{
-                                        return null;
-                                    }
-                                }):
-                                <Paper>
-                                    <Typography variant='body2'>
-                                        Not found ...
-                                    </Typography>
-                                </Paper>
-                            }
-                        </List>
+                                            </GridListTile>:null
+                                    )):
+                                    <Paper style={{width:'100%'}}>
+                                        <Typography variant='body2'>
+                                            Not found ...
+                                        </Typography>
+                                    </Paper>
+                                }
+                            </GridList>
+                        </div>
+                        <div style={{marginBottom:'100px'}}/>
                     </Scrollbars>
-                </div>
-                <Scrollbars
-                    renderThumbVertical={props => <div className={"dark-thumb-vertical"} {...props}/>}
-                >
-                    <div className={classes.configMainSection}>
-                        <GridList cellHeight={52} className={classes.configMainGridList} cols={12}>
-                            {filedShownCnt>0?
-                                tabs[currentTab]._fields.map((oneField:IConfigField, index:number)=>(
-                                    globalFilterString === '' || ( t(oneField._label) as string ).toLowerCase().indexOf(globalFilterString.toLowerCase())>-1?
-                                        <GridListTile key={index} cols={oneField._cols || 3} rows={oneField._rows || 1}>
-                                            <ConfigFieldComp
-                                                value={config[tabName][oneField._name]}
-                                                onChange={handleUpdateOneField(tabName,oneField._name)}
-                                                {...oneField}
-                                            />
-                                        </GridListTile>:null
-                                )):
-                                <Paper style={{width:'100%'}}>
-                                    <Typography variant='body2'>
-                                        Not found ...
-                                    </Typography>
-                                </Paper>
-                            }
-                        </GridList>
-                    </div>
-                    <div style={{marginBottom:'100px'}}/>
-                </Scrollbars>
-            </SplitterLayout>
-        </div>
+                </SplitterLayout>
+            </div>
+        </MuiPickersUtilsProvider>
     </React.Fragment>);
 }
 
